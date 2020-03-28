ced4ee3fa03fe4104149f879a76d1603b02fafc1
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2020-03-28 05:34:08 -0300

    Register page

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
+import heroes from '../../assets/heroes.png';
+import logo from '../../assets/logo.svg';
 
-export default function Logon() {
-    return (
-        <h1>Hello World</h1>
-    );
+
+function App() {
+  return (
+      <div className="logon-container">
+        <section className="form">
+          <img src={logo} alt="Be The Hero" />
+
+          <form>
+            <h1>Faca seu Logon</h1>
+
+            <input placeholder="Sua ID" />
+            <button className="button" type="submit">Entrar</button>
+
+            <Link className="back-link" to="/register">
+              <FiLogIn  size={16} color="#E02041" />
+              Nao tenho cadastro
+            </Link>
+          </form>          
+        </section>
+    
+        <img src={heroes} alt="Heroes" />
+      </div>
+
+  );
 }
+
+
+export default App;
